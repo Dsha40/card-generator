d6fe510a0dd19b5c7e5fe68a4b632f595d5571c0
Daniel Arias <danielcfc3008@gmail.com>
2021-05-09 19:38:28 -0430

    carta lista

--- src/app.js
+++ src/app.js
@@ -1,40 +1,89 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  let randomNumber = Math.random() * 10;
-  document.querySelector("#theExcuse").innerHTML = generateExcuse();
-  console.log("Hello Rigo from the console!");
-};
+  let sym = ["&#9824;", "&#9827;", "&#9829", "&#9830"];
+  let numCard = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  let cardNum = {
+    0: "A",
+    1: "2",
+    2: "3",
+    3: "4",
+    4: "5",
+    5: "6",
+    6: "7",
+    7: "8",
+    8: "9",
+    9: "10",
+    10: "J",
+    11: "Q",
+    12: "K"
+  };
 
-let generateExcuse = () => {
-  let who = ["The dog", "My grandma", "His turtle", "My bird"];
-  let action = ["ate", "peed", "crushed", "broke"];
-  let what = ["my homework", "the keys", "the car"];
-  let when = [
-    "before the class",
-    "right on time",
-    "when I finished",
-    "during my lunch",
-    "while I was praying"
-  ];
+  const numRandom = (num, arrSym) => {
+    let arrRandom = [];
+    arrRandom.push(Math.floor(Math.random() * num.length));
+    arrRandom.push(Math.floor(Math.random() * arrSym.length));
+    return arrRandom;
+  };
 
-  let goIndexWho = Math.floor(Math.random() * who.length);
-  let goIndexAction = Math.floor(Math.random() * action.length);
-  let goIndexWhat = Math.floor(Math.random() * what.length);
-  let goIndexWhen = Math.floor(Math.random() * when.length);
+  const creatorCard = (objCarta, pintaCarta, arrRandom) => {
+    let card = [];
+    card.push(objCarta[arrRandom[0]]);
+    card.push(pintaCarta[arrRandom[1]]);
+    return card;
+  };
 
-  return (
-    who[goIndexWho] +
-    " " +
-    action[goIndexAction] +
-    " " +
-    what[goIndexWhat] +
-    " " +
-    when[goIndexWhen]
-  );
+  const htmlCard = arregloFinal => {
+    let insertar = document.querySelector(".contenido");
+
+    if (insertar.childElementCount !== 0) {
+      insertar.removeChild(insertar.childNodes[0]);
+    }
+
+    let divCarta = document.createElement("div");
+    divCarta.className = "card";
+
+    let pintaArriba = document.createElement("div");
+    pintaArriba.className = "header";
+    pintaArriba.innerHTML = arregloFinal[1];
+    if (pintaArriba.innerHTML === "♥") {
+      pintaArriba.style.color = "red";
+    } else if (pintaArriba.innerHTML === "♦") {
+      pintaArriba.style.color = "red";
+    }
+    divCarta.appendChild(pintaArriba);
+
+    let contenido1 = document.createElement("h1");
+    contenido1.innerHTML = arregloFinal[0];
+    if (pintaArriba.innerHTML === "♥") {
+      contenido1.style.color = "red";
+    } else if (pintaArriba.innerHTML === "♦") {
+      contenido1.style.color = "red";
+    }
+    divCarta.appendChild(contenido1);
+
+    let pintaAbajo = document.createElement("div");
+    pintaAbajo.className = "footer";
+    pintaAbajo.innerHTML = arregloFinal[1];
+    if (pintaAbajo.innerHTML === "♥") {
+      pintaAbajo.style.color = "red";
+    } else if (pintaAbajo.innerHTML === "♦") {
+      pintaAbajo.style.color = "red";
+    }
+    divCarta.appendChild(pintaAbajo);
+
+    insertar.appendChild(divCarta);
+  };
+
+  let numeros = numRandom(numCard, sym);
+  let carta = creatorCard(cardNum, sym, numeros);
+  htmlCard(carta);
+
+  setInterval(() => {
+    let numeros = numRandom(numCard, sym);
+    let carta = creatorCard(cardNum, sym, numeros);
+    htmlCard(carta);
+  }, 5000);
 };
